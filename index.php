b7741e56cef686525e9ef697a8d9aff7383ddf45
Akshata Sharma <40199018+dramaticdeveloper@users.noreply.github.com>
2018-06-28 00:05:14 +0530

    Add files via upload

---
+++ index.php
@@ -0,0 +1,249 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <!-- The above 3 meta tags *must* come first in the head; any other head content must come *after* these tags -->
+    <title>Yeppo</title>
+
+    <!-- Bootstrap -->
+    <link href="css/bootstrap.min.css" rel="stylesheet">
+    
+    <!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
+    <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+    <!--[if lt IE 9]>
+      <script src="https://oss.maxcdn.com/html5shiv/3.7.3/html5shiv.min.js"></script>
+      <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+    <![endif]-->
+    <link href="https://fonts.googleapis.com/css?family=Homemade+Apple|Montserrat:300,700,800" rel="stylesheet">
+    <link rel="stylesheet" type="text/css" href="css/custom.css">
+    <link rel="stylesheet" type="text/css" href="css/animate.css">
+    <link rel="stylesheet" type="text/css" href="css/owl.carousel.min.css">
+    <link rel="stylesheet" type="text/css" href="css/owl.theme.default.min.css">
+
+  </head>
+  <body>
+    <nav class="navbar navbar-default navbar-fixed-top">
+      <div class="container">
+    <!-- Brand and toggle get grouped for better mobile display -->
+        <div class="navbar-header">
+          <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1" aria-expanded="false">
+            <span class="sr-only">Toggle navigation</span>
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+          </button>
+          <a class="navbar-brand" href="#">Yeppo</a>
+        </div>
+
+        <!-- Collect the nav links, forms, and other content for toggling -->
+        <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+          <ul class="nav navbar-nav navbar-right">
+            <li><a href="#">Home</a></li>
+            <li><a href="#">About</a></li>
+            <li><a href="#">gallery</a></li>
+            <li><a href="#">contact</a></li>
+          </ul>
+        </div><!-- /.navbar-collapse -->
+      </div><!-- /.container-fluid -->
+    </nav>
+    <!--banner area-->
+    <div class="banner">
+      <div class="container-fluid">
+       <div class="row">
+        <div class="col-md-2 animated slideInDown">
+         <img src="img/modell6.jpg" class="img-responsive" style="height: 90vh">
+        </div>
+        <div class="col-md-4 animated slideInUp">
+         <img src="img/modell1.jpg" class="img-responsive" style="height: 100vh">
+        </div>
+        <div class="col-md-3 animated slideInDown">
+         <img src="img/modell5.jpg" class="img-responsive" style="height: 85vh">
+        </div>
+        <div class="col-md-3 animated slideInUp">
+         <img src="img/modell2.jpg" class="img-responsive" style="height: 95vh">
+        </div>
+       </div>
+     </div>
+    </div>
+    <!-- Discover style column-->
+    <div class="container">
+      <div class="dstyle">
+        <div class="row">
+          <div class="col-md-6">
+            <h2>Discover</h2>
+            <h1>style</h1>
+            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+            tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+            quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+            consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
+            cillum dolore eu fugiat nulla pariatur.</p>
+          </div>
+          <div class="col-md-6">
+            <img src="img/style1.jpg" class="img-responsive">
+          </div>
+        </div>
+      </div>
+    </div>
+    <!--one liner area-->
+    <div class="oneliner">
+      <h2>vugfvuvfuhjvfujdvjvffgvuvuevudvgdhdf bhfvcehguvhu herbfhuev</h2>
+    </div>
+    <!-- latest trends-->
+    <div class="container">
+      <div class="trends">
+        <div class="row">
+          <div class="col-md-6">
+            <img src="img/style1.jpg" class="img-responsive">
+          </div>
+          <div class="col-md-6">
+            <h2>Latest</h2>
+            <h1>trends</h1>
+            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+            tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+            quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+            consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
+            cillum dolore eu fugiat nulla pariatur.</p>
+          </div>
+        </div>
+      </div>
+    </div>
+    <div class="container">
+      <div class=" we">
+        <h2>Who are</h2>
+        <h1>we?</h1>
+        <div class="row">
+          <div class="col-md-4">
+            <img src="https://png.icons8.com/ios/100/ed91a0/sewing-machine-filled.png">
+            <h2>Personal Designer</h2>
+            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+            tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+            </p>
+          </div>
+          <div class="col-md-4">
+            <img src="https://png.icons8.com/ios/100/ed91a0/wedding-dress-filled.png">
+            <h2>Fashion Stylist</h2>
+            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+            tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+            </p>
+          </div>
+          <div class="col-md-4">
+            <img src="https://png.icons8.com/ios/100/ed91a0/mirror-filled.png">
+            <h2>Beauty Guru</h2>
+            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+            tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+            </p>
+          </div> 
+        </div>
+      </div>
+    </div>  
+    <!-- photo gallery-->
+  <div class="container"> 
+    <div class="photogallery">  
+      <h2>Sneek Peek To Our</h2>
+      <h1>collection</h1>
+      <div class="owl-carousel owl-theme">
+        <div class="item"><h4>1</h4></div>
+        <div class="item"><h4>2</h4></div>
+        <div class="item"><h4>3</h4></div>
+        <div class="item"><h4>4</h4></div>
+        <div class="item"><h4>5</h4></div>
+        <div class="item"><h4>6</h4></div>
+        <div class="item"><h4>7</h4></div>
+        <div class="item"><h4>8</h4></div>
+        <div class="item"><h4>9</h4></div>
+        <div class="item"><h4>10</h4></div>
+        <div class="item"><h4>11</h4></div>
+        <div class="item"><h4>12</h4></div>
+      </div>   
+
+    </div>
+  </div>
+  <!--store-->
+  <div class="container">
+    <div class="drop">
+      <div class="row">
+        <div class="col-md-4">
+          <img src="https://png.icons8.com/ios/300/ed91a0/worldwide-location-filled.png">
+        </div>
+        <div class="col-md-8">
+          <h2>Drop By At Our</h2>
+          <h1>store</h1>
+          <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+          tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+         </p>
+        </div>
+      </div>
+    </div>
+ </div>
+  <!--footer-->
+  <div class="footer">
+    <div class="container">
+      <div class="row">
+       <div class="col-md-2">
+        <h5>location</h5>
+          <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+          tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+         
+          </p>
+        </div>
+        <div class="col-md-2">
+          <h5>posts</h5>
+          <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+          tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+         
+          </p>   
+        </div>
+        <div class="col-md-2">
+          <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+          tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+          quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+          
+          </p>
+        </div>
+        <div class="col-md-6">
+            <h2>Yeppo</h2>
+            <div class="imge">
+              <a href="#"><img src="https://png.icons8.com/android/40/ed91a0/facebook.png"></a>
+              <a href="#"><img src="https://png.icons8.com/ios/50/ed91a0/instagram-new-filled.png"></a>
+              <a href="#"><img src="https://png.icons8.com/ios/50/ed91a0/linkedin-filled.png"></a>
+              <a href="#"><img src="https://png.icons8.com/material/55/ed91a0/twitter.png"></a>
+              <a href="#"><img src="https://png.icons8.com/metro/40/ed91a0/tumblr.png"></a>
+            </div>
+        </div> 
+      </div>
+    </div>
+  </div>
+  <!--credits-->
+  <div class="credits">
+    Website Designed by Akshata Sharma
+  </div>
+
+    <!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
+    <!-- Include all compiled plugins (below), or include individual files as needed -->
+    
+    <script src="js/bootstrap.min.js"></script>
+    <script src="js/owl.carousel.min.js"></script>
+    <!-- <script src="js/jquery.min.js"></script> -->
+<script>
+      $('.owl-carousel').owlCarousel({
+    loop:true,
+    margin:10,
+    nav:true,
+    responsive:{
+        0:{
+            items:1
+        },
+        600:{
+            items:2
+        },
+        1000:{
+            items:3
+        }
+    }
+})
+    </script>
+  </body>
+</html>
